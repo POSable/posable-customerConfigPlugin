8faae36349a83999c84f7020abe4221d089a12da
Tristan Rubadeau <tristan@rubadeau.com>
2015-12-15 14:33:41 -0700

    update to the lookupfunction

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ var mongoose = require('mongoose');
 function ConfigPlugin () {
     mongoose.connect('mongodb://localhost/paymentData');
 
-    this.merchantLookup = function(internalID){
-          return Merchant.find({customerID: internalID});
-    }
+    //var db = mongoose.connection;
+    //db.on('error', console.error.bind(console, 'connection error:'));
+    //db.once('open', function (callback) {
+    //    console.log('connected');
+    //});
+
+    this.merchantLookup = function(internalID, callback){
+        Merchant.findOne( {internalID: internalID}, '', callback );
+    };
 }
 
 module.exports = new ConfigPlugin;
