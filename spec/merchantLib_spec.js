e538d642eed841d1a18c8218fa19bb5d3302ff6a
Nick Peters <nick@barbetsapp.com>
2016-02-29 16:44:49 -0700

    add test specs for merchantLib crud actions

--- spec/merchantLib_spec.js
+++ spec/merchantLib_spec.js
@@ -3,61 +3,92 @@ var testMerchantModel;
 var testCounterModel;
 var testJWT;
 var testMerchantMap;
-//var testMerchantCounter = function(callback) { return callback(null, 1) };
-var initialize;
-var testCB = function(arg1, arg2){return {err: arg1, res: arg2}};
+var testMerchantCounter;
+var testNewMerchObj;
 var testLogger;
-var testNewMerchObj = {name: 'testMerchant'};
+var testCB;
+
 
 describe('merchantLib', function(){
-
-    describe('saves new merchants to database: ', function(){
-
-        beforeEach(function(){
-            testCounterModel = {findByIdAndUpdate: function(){ return testCB(null, 4)} };
-            //testMerchantModel = {findOneAndRemove: function(){}, update: function(){}, save: function(){}, findOne: function(){}};
-            testMerchantMap = function(newMerchantObj, callback) { return initialize };
-            //testMerchantCounter = {findByIdAndUpdate: function(callback){return callback(null, 1)}};
-            testJWT = {sign: function(){}};
-            testLogger = {error: function(){}, debug: function(){}};
-            testMerchantLib.testStub(testCounterModel, testMerchantModel, testJWT, testLogger);
-
-            spyOn(testCounterModel, 'findByIdAndUpdate');
-            //spyOn(testMerchantModel, 'findOneAndRemove');
-            //spyOn(testMerchantModel, 'update');
-            //spyOn(testMerchantModel, 'save');
-            //spyOn(testMerchantModel, 'findOne');
-            spyOn(initialize, 'merchantCounter');
-            spyOn(testJWT, 'sign');
-            spyOn(testLogger, 'debug');
-            spyOn(testLogger, 'error');
-
-            //testMerchantLib.merchantCreate({name: 'testMerchant'}, testCB);
-        });
-
-        //it('finds merchant counter record and updates/increments internalID', function(){
-        //    testMerchantLib.merchantCreate(testNewMerchObj, testCB);
-        //    expect(testCounterModel.findByIdAndUpdate).toHaveBeenCalled();
-        //});
-        //
-        //it('starts mapping new merchants by calling merchantCounter', function(){
-        //    testMerchantMap(testNewMerchObj, testCB);
-        //    expect(testMerchantMap.testMerchantCounter).toHaveBeenCalled();
-        //});
-        //
-        //it('assigns a new posapi token to new merchants', function(){
-        //    expect(testJWT.sign).toHaveBeenCalled();
-        //});
-
-
-        //it('merchantCounter that finds/updates merchant counter', function(){
-        //    expect(testCounterModel.findByIdAndUpdate).toHaveBeenCalled();
-        //});
-        //
-        //it('merchantMap that calls merchantCounter and maps new merchant', function(){
-        //    expect(testJWT.sign).toHaveBeenCalled();
-        //});
-
+    beforeEach(function(){
+        testNewMerchObj = {name: 'testMerchant', internalID: 1, save: function(){}};
+        testLogger = {error: function(){}, debug: function(){}};
+        testCB = function(arg1, arg2){return {err: arg1, res: arg2}};
     });
 
+    describe('starts mapping new merchants by', function(){
+        beforeEach(function(){
+            testCounterModel = {findByIdAndUpdate: function(){}};
+            testMerchantLib.testStub(testCounterModel);
+
+            spyOn(testCounterModel, 'findByIdAndUpdate');
+        });
+
+        it('finding/updating the merchant counter record to increment the internalID', function(){
+            testMerchantLib.merchantCreate(testNewMerchObj, testCB);
+            expect(testCounterModel.findByIdAndUpdate).toHaveBeenCalled();
+        });
+    });
+
+    describe('continues mapping', function(){
+        beforeEach(function(){
+            testJWT = {sign: function(){}};
+            testMerchantCounter = function(callback) { return callback(null, 1)};
+            testMerchantLib.testStub(null, null, testJWT, testLogger, null, testMerchantCounter);
+
+            spyOn(testJWT, 'sign');
+
+            testMerchantLib.merchantCreate(testNewMerchObj, testCB);
+        });
+
+        it('assigns a new posapi token to new merchants', function(){
+            expect(testJWT.sign).toHaveBeenCalledWith({name: 'testMerchant', internalID: 1}, 'posapiToken');
+        });
+    });
+
+    describe('finishes mapping by returning a merchant object', function(){
+        beforeEach(function(){
+            testMerchantMap = function(arg1, callback) { return callback(null, testNewMerchObj); };
+            testMerchantLib.testStub(null, null, null, testLogger, testMerchantMap);
+
+            spyOn(testNewMerchObj, 'save');
+            spyOn(testLogger, 'debug');
+
+            testMerchantLib.merchantCreate({}, testCB);
+        });
+
+        it('should return a new properly mapped merchant', function(){
+            expect(testLogger.debug).toHaveBeenCalledWith('New merchant mapped successfully, saving...');
+        });
+
+        it('should call save on the new merchant', function(){
+            expect(testNewMerchObj.save).toHaveBeenCalled();
+        });
+    });
+
+    describe('is able to perform search/CRUD actions for merchants: ', function(){
+        beforeEach(function(){
+            testMerchantModel = {findOneAndRemove: function(){}, update: function(){}, findOne: function(){}};
+            testMerchantLib.testStub(null, testMerchantModel, null, testLogger);
+
+            spyOn(testMerchantModel, 'findOne');
+            spyOn(testMerchantModel, 'findOneAndRemove');
+            spyOn(testMerchantModel, 'update');
+        });
+
+        it('finds existing merchants', function(){
+            testMerchantLib.merchantFind();
+            expect(testMerchantModel.findOne).toHaveBeenCalled();
+        });
+
+        it('updates existing merchants', function(){
+            testMerchantLib.merchantModify(testNewMerchObj);
+            expect(testMerchantModel.update).toHaveBeenCalled();
+        });
+
+        it('deletes existing merchants', function(){
+            testMerchantLib.merchantRemove(testNewMerchObj);
+            expect(testMerchantModel.findOneAndRemove).toHaveBeenCalled();
+        });
+    });
 });
